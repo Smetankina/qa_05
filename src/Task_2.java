0ea6487ee21cb8c9e399dfb3af998909fa1be909
Anna <an.kunitskaya@gmail.com>
2022-09-08 08:03:13 +0300

    Initial commit

---
+++ src/Task_2.java
@@ -0,0 +1,40 @@
+public class Task_2 {
+
+    public static void main(String[] args) {
+
+        int a = 6;
+
+        int b = 5;
+
+        int sum = a+b;
+
+        int mul = a*b;
+
+        int div = a/b;
+
+        int w = a%b;
+
+        if(a % 2 == 0) {
+            System.out.println(a  + " is even" );
+        }
+            else{
+                System.out.println(a + "is odd");
+            }
+        if (b%2 == 0){
+            System.out.println( b +"is even");
+        }
+        else {
+            System.out.println(b + "is odd");
+        }
+
+
+
+        System.out.println("\u263a");
+        }
+
+
+
+
+
+    }
+
